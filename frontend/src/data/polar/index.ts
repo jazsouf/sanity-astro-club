b5b39b1d870f27d5362478c91f68d9d418017c21
jazsouf <101473962+jazsouf@users.noreply.github.com>
2025-04-22 17:27:46 +0200

    working checkout

--- frontend/src/data/polar/index.ts
+++ frontend/src/data/polar/index.ts
@@ -1,7 +1,7 @@
 import { POLAR_ACCESS_TOKEN } from "astro:env/server";
 
 const getOptions = {method: 'GET', headers: {Authorization: `Bearer ${POLAR_ACCESS_TOKEN}`}};
-const postOptions = {method: 'POST', headers: {Authorization: `Bearer ${POLAR_ACCESS_TOKEN}`}};
+const postOptions = {method: 'POST', headers: {Authorization: `Bearer ${POLAR_ACCESS_TOKEN}`, 'Content-Type': 'application/json' }};
 
 export async function getProductById(sku: string) {
 
@@ -19,16 +19,24 @@ export async function getProductById(sku: string) {
   }
 }
 
-export async function getCheckoutSession(skus: string[]) {
+export async function getCheckoutUrl(skus: string[]) {
+
+
   const body = JSON.stringify({
-      products: skus // Use "products" as the key instead of "skus"
+    allow_discount_codes :true,
+    require_billing_address :false,
+    products: skus
     });
 
-  const res = await fetch('https://api.polar.sh/v1/checkouts/', {...postOptions, body: JSON.stringify({skus})})
+  const res = await fetch('https://api.polar.sh/v1/checkouts/', {...postOptions, body})
     .then(response => response.json())
-    .then(response => console.log(response))
     .catch(err => console.error(err));
 
-  console.log({res});
 
+  // return {
+  //   url: res.url,
+  //   success_url: res.success_url,
+  //   id: res.id,
+  // }
+ return res.url
 }
