f3037c72089f3470b5e4594cfc68988d3d080aad
jazsouf <101473962+jazsouf@users.noreply.github.com>
2025-04-23 17:28:42 +0200

    wip

--- frontend/src/store/cart.ts
+++ frontend/src/store/cart.ts
@@ -1,1 +1,28 @@
-//TODO: store items with nanostores and persist in something like localStorage
+import { atom, map } from 'nanostores';
+
+export const isCartOpen = atom(false);
+
+export type CartItem = {
+  id: string;
+  title: string;
+  imageRef: string;
+  quantity: number;
+}
+
+export const cartItems = map<Record<string, CartItem>>({});
+
+type ItemDisplayInfo = Pick<CartItem, 'id' | 'title' | 'imageRef'>;
+export function addCartItem({ id, title, imageRef }: ItemDisplayInfo) {
+  const existingEntry = cartItems.get()[id];
+  if (existingEntry) {
+    cartItems.setKey(id, {
+      ...existingEntry,
+      quantity: existingEntry.quantity + 1,
+    });
+  } else {
+    cartItems.setKey(
+      id,
+      { id, title, imageRef, quantity: 1 }
+    );
+  }
+}
